ba4d375b86945e887837bfa3c67c3c0c6c90b794
Amol Gawai <Amol.Gawai@doesnot.exist>
2020-03-18 14:30:50 +0100

    auto header documentation

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,2 +1,34 @@
+;;; .emacs-profiles.el --- profiles tobe used with chemacs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Amol Gawai
+
+;; Author: Amol Gawai(("default" . ((user-emacs-directory . "~/.emacs.d"))) <amol@doesnot.exist>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; chemacs provides ability to use different Emacs "distros".  It is done through having
+;; differnet "profiles" which are provided in this file
+;;
+
+;;; Code:
+;; FIXME:- change the doom-emacs directory to ~/code/emacs-distros/doom-emacs
+
 (("default" . ((user-emacs-directory . "~/.emacs.d")))
  ("doom" .((user-emacs-directory . "~/emacs-distros/doom-emacs"))))
+
+(provide '.emacs-profiles)
+;;; .emacs-profiles.el ends here
